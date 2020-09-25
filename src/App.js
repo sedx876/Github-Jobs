d3ccc7ef873dbaad2d628d0b061325a8cbc91573
sedx876 <swatkins876@gmail.com>
2020-09-25 06:48:55 -0400

    added Job.js component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/App.css';
 import useFetchJobs from './components/useFetchJobs'
+import Job from './components/Job'
 import { Container } from 'react-bootstrap'
 
 function App() {
 
-  const { jobs, loading, error } = useFetchJobs()
+  const [params, setParams] = useState({})
+  const [page, setPage] = useState(1)
+  const { jobs, loading, error } = useFetchJobs(params, page)
+  
+  
 
   return (
     <Container>
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error, Try Refreshing...</h1>}
-      <h1>{jobs.length}</h1>
+      {jobs.map(job => {
+        return <Job key={job.id} job={job} />
+      })}
     </Container>
   );
 }
--- src/components/useFetchJobs.js
+++ src/components/useFetchJobs.js
@@ -1,15 +1,13 @@
-import Axios from 'axios'
-import {useReducer, useEffect} from 'react'
+import { useReducer, useEffect } from 'react'
+import axios from 'axios'
 
 const ACTIONS = {
   MAKE_REQUEST: 'make-request',
   GET_DATA: 'get-data',
-  ERROR: 'error'
+  ERROR: 'error',
+  UPDATE_HAS_NEXT_PAGE: 'update-has-next-page'
 }
 
-
-//'http://cors-anywhere.herokuapp.com/   ---put this in front of 
-//URL to avoid CORS errors (work around)
 const BASE_URL = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json'
 
 function reducer(state, action) {
@@ -28,19 +26,37 @@ function reducer(state, action) {
 }
 
 export default function useFetchJobs(params, page) {
-
-  const [state, dispatch] = useReducer(reducer, {jobs: [], loading: true })
+  const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true })
 
   useEffect(() => {
+    const cancelToken1 = axios.CancelToken.source()
     dispatch({ type: ACTIONS.MAKE_REQUEST })
-    Axios.get(BASE_URL, {
+    axios.get(BASE_URL, {
+      cancelToken: cancelToken1.token,
       params: { markdown: true, page: page, ...params }
     }).then(res => {
-      dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data } })
+      dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data } }) 
     }).catch(e => {
-      dispatch({ type: ACTIONS.ERROR, payload: { error: e } })
+      if (axios.isCancel(e)) return
+      dispatch({ type: ACTIONS.ERROR, payload: { error: e } }) 
     })
-  }, [params, page])
 
+    const cancelToken2 = axios.CancelToken.source()
+    axios.get(BASE_URL, {
+      cancelToken: cancelToken2.token,
+      params: { markdown: true, page: page + 1, ...params }
+    }).then(res => {
+      dispatch({ type: ACTIONS.UPDATE_HAS_NEXT_PAGE, payload: { hasNextPage: res.data.length !== 0 } }) 
+    }).catch(e => {
+      if (axios.isCancel(e)) return
+      dispatch({ type: ACTIONS.ERROR, payload: { error: e } }) 
+    })
+
+    return () => {
+      cancelToken1.cancel()
+      cancelToken2.cancel()
+    }
+  }, [params, page])
+  
   return state
 }
